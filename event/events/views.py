96d4393f632b3e000eb79abb3e314a0af9b02b89
Booki Park <bkbkgg@gmail.com>
2018-09-18 17:16:16 +0900

    detail view에 form 추가 및 event apply 뷰 추가

--- event/events/views.py
+++ event/events/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from events.forms import ApplicationForm
 from events.models import Event
 
 
@@ -13,7 +14,20 @@ def event_list(request):
 
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
+    application_form = ApplicationForm()
     context = {
         'event': event,
+        'form': application_form,
     }
     return render(request, 'event_detail.html', context)
+
+
+def event_apply(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            application = form.save(commit=False)
+            application.event = event
+            application.save()
+            return redirect('event-detail', pk=event.pk)
